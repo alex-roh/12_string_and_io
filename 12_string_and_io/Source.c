241fb8b2581506f86d5bf69f6679f34ae566a948
eunyoungroh <roheunyoung@outlook.com>
2019-11-21 15:21:45 +0900

    실습 5_2
    오류가 발생하는 코드.

--- 12_string_and_io/Source.c
+++ 12_string_and_io/Source.c
@@ -5,15 +5,14 @@
 int main() {
 	
 	FILE* file_ptr;
-	file_ptr = fopen("sookmyung.txt", "w");
+	file_ptr = fopen("sookmyung.txt", "r");
 	char text[MAXLENGTH];
 
 	int i;
 	for (i = 0; i < 3; i++)
 	{
-		printf("input a word: ");
-		scanf("%s", &text);
-		fprintf(file_ptr, "%s\n", text);
+		fscanf_s(file_ptr, "%s", text);
+		printf("%s\n", text);
 	}
 
 	fclose(file_ptr);
